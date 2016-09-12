30c5fa7e353952d51c6ba987dc8867455bb4f34c
Colin McDonald <mickydwow@gmail.com>
2016-09-12 15:19:04 -0700

    Added resume

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express = require('express')
+var express = require('express');
+var fs = require('fs');
 
-var app = express()
+var app = express();
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(port, function(err, success) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log('Listening in on: ', port)
+    console.log('Listening in on: ', port);
   }
-})
+});
